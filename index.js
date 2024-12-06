0b45dd4db0298cfda153e4cf66d9fe7ba902389a
cloverche <sunny40224@gmail.com>
2024-12-06 20:00:56 +0800

    解錯誤

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const productsRouter = require('./src/routes/products')
+const productsRouter = require('./src/routes/productsList')
 const registerLoginRouter = require('./src/routes/registerLoginAuth')
+const googleAuthRouter = require('./src/routes/googleAuth')
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173', // 前端網址，之後佈署了要改
+  methods: ['POST', 'GET', 'OPTIONS'],
+  credentials: true
+}));
 
+// 加入這些安全標頭，嘗試解決 CORS 問題
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+// 設定靜態檔案服務，讓前端可以來取後端 images 資料夾ㄉ檔案
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use(express.json())
-app.use('/products', productsRouter)
+app.use('/categories', productsRouter)
 app.use('/users',registerLoginRouter)
+app.use('/auth', googleAuthRouter)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
