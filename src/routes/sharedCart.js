e7c3616d6dd37fd6ba8f7ae3412b325bc4384490
33-Fang <33justworkplace@gmail.com>
2024-12-14 22:21:34 +0800

    feat: 取得特定共享購物車的產品列表

--- src/routes/sharedCart.js
+++ src/routes/sharedCart.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const prisma = require('../configs/prisma');
-const { object } = require('zod');
-const e = require('express');
 
 // 取得共享購物車列表
 router.get('/sharedCartList', async (req, res) => {
@@ -46,19 +44,19 @@ router.get('/sharedCartList', async (req, res) => {
         })
         memberUserId = memberUserId.map((object) => object["user_id"])
         let member = []
-        for(let i = 0; i < memberUserId.length; i++){
+        for (let i = 0; i < memberUserId.length; i++) {
           const username = await prisma.users.findUnique({
-            where:{
+            where: {
               userId: memberUserId[i]
             },
             select: {
               username: true
             }
           })
-          member.push({username:username["username"]})
+          member.push({ username: username["username"] })
         }
         member = member.map((object) => object.username)
-        sharedCartList.push({ id: groupIdList[i], name: sharedCartName["name"], member})
+        sharedCartList.push({ id: groupIdList[i], name: sharedCartName["name"], member })
       }
       res.json({
         sharedCartList,
@@ -68,6 +66,73 @@ router.get('/sharedCartList', async (req, res) => {
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: "伺服器錯誤" })
+  }
+})
+
+
+// 取得特定共享購物車的產品列表
+router.get('/sharedCartItem/:groupId?', async (req, res) => {
+  const { groupId } = req.params
+  // const userId = req.headers
+  const userId = "d9ee8caa-3dd2-4ca3-b72b-e0edfd19ae22"
+  try {
+    const belongBy = await prisma.shared_cart_users.findMany({
+      where: {
+        shared_cart_group_id: parseFloat(groupId)
+      },
+      select: {
+        user_id: true
+      }
+    })
+    const belongByUserlist = []
+    for (let i = 0; i < belongBy.length; i++) {
+      belongByUserlist.push(belongBy[i]["user_id"])
+    }
+    const found = belongByUserlist.find((user) => user == userId)
+    if (found) {
+      let productIdList = await prisma.shared_carts.findMany({
+        where: {
+          group_id: parseFloat(groupId)
+        },
+        select: {
+          product_id: true
+        }
+      })
+      productIdList = productIdList.map((object) => object["product_id"])
+      const productDataList = []
+      for (let i = 0; i < productIdList.length; i++) {
+        const productData = await prisma.products.findUnique({
+          where: {
+            product_id: productIdList[i]
+          },
+        })
+        const imgPath = await prisma.product_images.findFirst({
+          where: {
+            product_id: productIdList[i],
+            image_type: "main",
+            order_sort: 1
+          },
+          select: {
+            image_path: true
+          }
+        })
+        const productQty = await prisma.shared_carts.findFirst({
+          where: {
+            product_id: productIdList[i],
+            group_id: parseFloat(groupId)
+          },
+          select: {
+            quantity: true
+          }
+        })
+        productDataList.push({ ...productData, ...imgPath, ...productQty })
+      }
+      res.json(productDataList)
+    } else {
+      res.status(403).json({ message: "用戶沒有訪問這個共享購物車的權限" })
+    }
+  } catch (err){
+    res.status(500).json("伺服器怪怪了")
   }
 })
 
