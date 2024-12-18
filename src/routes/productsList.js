cacf3d073af4a2cffe7d8f15aaf2b81acbe69f0c
33-Fang <33justworkplace@gmail.com>
2024-12-18 14:58:31 +0800

    chore: #25 修改引入方式從 require 變 import、調整 images 檔案位置

--- src/routes/productsList.js
+++ src/routes/productsList.js
@@ -1,120 +1,120 @@
-const express = require('express')
+import express from "express"
+import prisma from "../configs/prisma.js"
 const router = express.Router()
-const prisma = require('../configs/prisma')
-
-const API_URL = "http://localhost:3300/"
 
 // 因為格式化商品資料用了兩次所以提出來
 const formatProducts = (products) => {
-  return products.map(product => ({
-    id: product.product_id,
-    title: product.product_name,
-    price: Number(product.sale_price),
-    originalPrice: Number(product.original_price),
-    frontImg: getImageUrl(product.product_images[0]?.image_path),
-    backImg: getImageUrl(product.product_images[1]?.image_path)
-  }))
+	return products.map((product) => ({
+		id: product.product_id,
+		title: product.product_name,
+		price: Number(product.sale_price),
+		originalPrice: Number(product.original_price),
+		frontImg: getImageUrl(product.product_images[0]?.image_path),
+		backImg: getImageUrl(product.product_images[1]?.image_path),
+	}))
 }
 
 // 圖片路徑
-const getImageUrl = (imagePath) => {
-  if (!imagePath) return ''
-  return `${API_URL}${imagePath}`
-}
+const getImageUrl = (imagePath) => `${process.env.API_URL}${imagePath}`
 
 // 排序映射
-const listOrderBy = { latest: { listed_at: 'desc' }, oldest: { listed_at: 'asc' }, expensive: { sale_price: 'desc' }, cheap: { sale_price: 'asc' }, popular: { total_sales: 'desc' } }
+const listOrderBy = {
+	latest: { listed_at: "desc" },
+	oldest: { listed_at: "asc" },
+	expensive: { sale_price: "desc" },
+	cheap: { sale_price: "asc" },
+	popular: { total_sales: "desc" },
+}
 
 // API 主戰場!
-router.get('/:categoryId?', async (req, res) => {
-  const { sortBy, pageSize, pageNum } = req.query
+router.get("/:categoryId?", async (req, res) => {
+	const { sortBy, pageSize, pageNum } = req.query
 
-  // 分頁相關
-const pageStart = (pageNum - 1) * pageSize
-const pageEnd = pageStart + Number(pageSize)
+	// 分頁相關
+	const pageStart = (pageNum - 1) * pageSize
+	const pageEnd = pageStart + Number(pageSize)
 
-  try {
-    // 確定 ID 是數字（轉一下）
-    const categoryId = req.params.categoryId ? parseInt(req.params.categoryId) : null
+	try {
+		// 確定 ID 是數字（轉一下）
+		const categoryId = req.params.categoryId
+			? parseInt(req.params.categoryId)
+			: null
 
-    // 如果沒有提供分類參數，取得所有商品
-    if (!categoryId) {
-      const products = await prisma.products.findMany({
-        orderBy: listOrderBy[sortBy] || { product_id: 'asc' } ,
-        include: {
-          product_images: {
-            where: {
-              AND: [
-                { image_type: 'main' },
-                { order_sort: { in: [1, 2] } }
-              ]
-            },
-            orderBy: {
-              order_sort: 'asc'
-            }
-          }
-        }
-      })
+		// 如果沒有提供分類參數，取得所有商品
+		if (!categoryId) {
+			const products = await prisma.products.findMany({
+				orderBy: listOrderBy[sortBy] || { product_id: "asc" },
+				include: {
+					product_images: {
+						where: {
+							AND: [{ image_type: "main" }, { order_sort: { in: [1, 2] } }],
+						},
+						orderBy: {
+							order_sort: "asc",
+						},
+					},
+				},
+			})
 
-      return res.json({
-        categoryName: "所有商品",
-        products: formatProducts(products).slice(pageStart, pageEnd) || formatProducts(products),
-        totalProduct:products.length
-      })
-    }
+			return res.json({
+				categoryName: "所有商品",
+				products:
+					formatProducts(products).slice(pageStart, pageEnd) ||
+					formatProducts(products),
+				totalProduct: products.length,
+			})
+		}
 
-    // 如果有提供分類參數，先查詢分類資訊
-    const categoryData = await prisma.categories.findUnique({
-      where: {
-        categories_id: categoryId
-      }
-    })
+		// 如果有提供分類參數，先查詢分類資訊
+		const categoryData = await prisma.categories.findUnique({
+			where: {
+				categories_id: categoryId,
+			},
+		})
 
-    // 如果沒有那個分類 ID，船錯誤
-    if (!categoryData) {
-      return res.status(404).json({
-        status: "Error",
-        message: "分類不存在"
-      })
-    }
+		// 如果沒有那個分類 ID，傳錯誤
+		if (!categoryData) {
+			return res.status(404).json({
+				status: "Error",
+				message: "分類不存在",
+			})
+		}
 
-    // 查詢該分類的商品
-    const products = await prisma.products.findMany ({
-      where: {
-        product_categories: {
-          some: {
-            category_id: categoryId
-          }
-        }
-      },
-      orderBy: listOrderBy[sortBy] || { product_id: 'asc' } ,
-      include: {
-        product_images: {
-          where: {
-            AND: [
-              { image_type: 'main' },
-              { order_sort: { in: [1, 2] } }
-            ]
-          },
-          orderBy: {
-            order_sort: 'asc'
-          }
-        }
-      }
-    })
+		// 查詢該分類的商品
+		const products = await prisma.products.findMany({
+			where: {
+				product_categories: {
+					some: {
+						category_id: categoryId,
+					},
+				},
+			},
+			orderBy: listOrderBy[sortBy] || { product_id: "asc" },
+			include: {
+				product_images: {
+					where: {
+						AND: [{ image_type: "main" }, { order_sort: { in: [1, 2] } }],
+					},
+					orderBy: {
+						order_sort: "asc",
+					},
+				},
+			},
+		})
 
-    res.json({
-      categoryName: categoryData?.category_name || "所有商品",
-      products: formatProducts(products).slice(pageStart, pageEnd) || formatProducts(products),
-      totalProduct:products.length
-    })
-
-  } catch (err) {
-    res.status(500).json({
-      status: "Error",
-      message: err.message
-    })
-  }
+		res.json({
+			categoryName: categoryData?.category_name || "所有商品",
+			products:
+				formatProducts(products).slice(pageStart, pageEnd) ||
+				formatProducts(products),
+			totalProduct: products.length,
+		})
+	} catch (err) {
+		res.status(500).json({
+			status: "Error",
+			message: err.message,
+		})
+	}
 })
 
-module.exports = router
+export default router
--- src/routes/sharedCart.js
+++ src/routes/sharedCart.js
@@ -1,193 +1,199 @@
-const express = require('express')
+import express from "express"
+import prisma from "../configs/prisma.js"
+import { v4 as uuidv4 } from "uuid"
 const router = express.Router()
-const prisma = require('../configs/prisma');
-const { v4: uuidv4 } = require('uuid')
 
 // 取得共享購物車列表
-router.get('/sharedCartList', async (req, res) => {
-  const userId = "d9ee8caa-3dd2-4ca3-b72b-e0edfd19ae22" // 先用固定資料測試
-  try {
-    // 如果有這個使用者
-    if (userId) {
-      // 用 userId 找到 group_id
-      let groupIdList = await prisma.shared_cart_users.findMany({
-        where: {
-          user_id: userId
-        },
-        select: {
-          shared_cart_group_id: true
-        }
-      })
-      // 取裡面的 shared_cart_group_id 欄位（不然會包在 {} 裡面
-      groupIdList = groupIdList.map((object) => object.shared_cart_group_id)
-      const sharedCartList = []
-      for (let i = 0; i < groupIdList.length; i++) {
-        // 找共享購物車名稱
-        const sharedCartName = await prisma.shared_carts.findFirst({
-          where: {
-            group_id: groupIdList[i]
-          },
-          select: {
-            name: true
-          }
-        })
-        // 找成員
-        let memberUserId = await prisma.shared_cart_users.findMany({
-          where: {
-            shared_cart_group_id: groupIdList[i],
-            user_id: {
-              not: userId
-            }
-          },
-          select: {
-            user_id: true
-          }
-        })
-        memberUserId = memberUserId.map((object) => object["user_id"])
-        let member = []
-        for (let i = 0; i < memberUserId.length; i++) {
-          const username = await prisma.users.findUnique({
-            where: {
-              userId: memberUserId[i]
-            },
-            select: {
-              username: true
-            }
-          })
-          member.push({ username: username["username"] })
-        }
-        member = member.map((object) => object.username)
-        sharedCartList.push({ id: groupIdList[i], name: sharedCartName["name"], member })
-      }
-      res.json({
-        sharedCartList,
-      })
-    }
-
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ message: "伺服器錯誤" })
-  }
+router.get("/sharedCartList", async (req, res) => {
+	const userId = "d9ee8caa-3dd2-4ca3-b72b-e0edfd19ae22" // 先用固定資料測試
+	try {
+		// 如果有這個使用者
+		if (userId) {
+			// 用 userId 找到 group_id
+			let groupIdList = await prisma.shared_cart_users.findMany({
+				where: {
+					user_id: userId,
+				},
+				select: {
+					shared_cart_group_id: true,
+				},
+			})
+			// 取裡面的 shared_cart_group_id 欄位（不然會包在 {} 裡面
+			groupIdList = groupIdList.map((object) => object.shared_cart_group_id)
+			const sharedCartList = []
+			for (let i = 0; i < groupIdList.length; i++) {
+				// 找共享購物車名稱
+				const sharedCartName = await prisma.shared_carts.findFirst({
+					where: {
+						group_id: groupIdList[i],
+					},
+					select: {
+						name: true,
+					},
+				})
+				// 找成員
+				let memberUserId = await prisma.shared_cart_users.findMany({
+					where: {
+						shared_cart_group_id: groupIdList[i],
+						user_id: {
+							not: userId,
+						},
+					},
+					select: {
+						user_id: true,
+					},
+				})
+				memberUserId = memberUserId.map((object) => object["user_id"])
+				let member = []
+				for (let i = 0; i < memberUserId.length; i++) {
+					const username = await prisma.users.findUnique({
+						where: {
+							userId: memberUserId[i],
+						},
+						select: {
+							username: true,
+						},
+					})
+					member.push({ username: username["username"] })
+				}
+				member = member.map((object) => object.username)
+				sharedCartList.push({
+					id: groupIdList[i],
+					name: sharedCartName["name"],
+					member,
+				})
+			}
+			res.json({
+				sharedCartList,
+			})
+		}
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).json({ message: "伺服器錯誤" })
+	}
 })
 
-
 // 取得特定共享購物車的產品列表
-router.get('/sharedCartItem/:groupId?', async (req, res) => {
-  const { groupId } = req.params
-  // const userId = req.headers
-  const userId = "d9ee8caa-3dd2-4ca3-b72b-e0edfd19ae22"
-  try {
-    const belongBy = await prisma.shared_cart_users.findMany({
-      where: {
-        shared_cart_group_id: groupId
-      },
-      select: {
-        user_id: true
-      }
-    })
-    const belongByUserlist = []
-    for (let i = 0; i < belongBy.length; i++) {
-      belongByUserlist.push(belongBy[i]["user_id"])
-    }
-    const found = belongByUserlist.find((user) => user == userId)
-    if (found) {
-      let productIdList = await prisma.shared_carts.findMany({
-        where: {
-          group_id: groupId
-        },
-        select: {
-          product_id: true
-        }
-      })
-      
-      productIdList = productIdList.map((object) => object["product_id"])      
-      if (productIdList[0] === null) {
-        res.json([])
-      } else {
-        const productDataList = []
-        for (let i = 0; i < productIdList.length; i++) {
-          const productData = await prisma.products.findUnique({
-            where: {
-              product_id: productIdList[i]
-            },
-          })
-          let imgPath = await prisma.product_images.findFirst({
-            where: {
-              product_id: productIdList[i],
-              image_type: "main",
-              order_sort: 1
-            },
-            select: {
-              image_path: true
-            }
-          })
-          imgPath = `http://localhost:3300/${imgPath.image_path}`
-          const productQty = await prisma.shared_carts.findFirst({
-            where: {
-              product_id: productIdList[i],
-              group_id: groupId
-            },
-            select: {
-              quantity: true
-            }
-          })
-          productDataList.push({ ...productData, image_path: imgPath, ...productQty })
-        }
-        res.json(productDataList)
-      }
-    } else {
-      res.status(403).json({ message: "用戶沒有訪問這個共享購物車的權限" })
-    }
-  } catch (err) {
-    res.status(500).json("伺服器怪怪了")
-  }
+router.get("/sharedCartItem/:groupId?", async (req, res) => {
+	const { groupId } = req.params
+	// const userId = req.headers
+	const userId = "d9ee8caa-3dd2-4ca3-b72b-e0edfd19ae22"
+	try {
+		const belongBy = await prisma.shared_cart_users.findMany({
+			where: {
+				shared_cart_group_id: groupId,
+			},
+			select: {
+				user_id: true,
+			},
+		})
+		const belongByUserlist = []
+		for (let i = 0; i < belongBy.length; i++) {
+			belongByUserlist.push(belongBy[i]["user_id"])
+		}
+		const found = belongByUserlist.find((user) => user == userId)
+		if (found) {
+			let productIdList = await prisma.shared_carts.findMany({
+				where: {
+					group_id: groupId,
+				},
+				select: {
+					product_id: true,
+				},
+			})
+
+			productIdList = productIdList.map((object) => object["product_id"])
+			if (productIdList[0] === null) {
+				res.json([])
+			} else {
+				const productDataList = []
+				for (let i = 0; i < productIdList.length; i++) {
+					const productData = await prisma.products.findUnique({
+						where: {
+							product_id: productIdList[i],
+						},
+					})
+					let imgPath = await prisma.product_images.findFirst({
+						where: {
+							product_id: productIdList[i],
+							image_type: "main",
+							order_sort: 1,
+						},
+						select: {
+							image_path: true,
+						},
+					})
+					imgPath = `http://localhost:3300/${imgPath.image_path}`
+					const productQty = await prisma.shared_carts.findFirst({
+						where: {
+							product_id: productIdList[i],
+							group_id: groupId,
+						},
+						select: {
+							quantity: true,
+						},
+					})
+					productDataList.push({
+						...productData,
+						image_path: imgPath,
+						...productQty,
+					})
+				}
+				res.json(productDataList)
+			}
+		} else {
+			res.status(403).json({ message: "用戶沒有訪問這個共享購物車的權限" })
+		}
+	} catch (err) {
+		res.status(500).json("伺服器怪怪了")
+	}
 })
 
 // 創建共享購物車
-router.post('/sharedCart', async (req, res) => {
-  const { sharedCartName, creatorUID, memberEmail } = req.body
-  const sharedCartGroupId = uuidv4()
+router.post("/sharedCart", async (req, res) => {
+	const { sharedCartName, creatorUID, memberEmail } = req.body
+	const sharedCartGroupId = uuidv4()
 
-  try {
-    const memberUserId = await prisma.users.findUnique({
-      where: {
-        email: memberEmail
-      },
-      select: {
-        userId: true
-      }
-    })
+	try {
+		const memberUserId = await prisma.users.findUnique({
+			where: {
+				email: memberEmail,
+			},
+			select: {
+				userId: true,
+			},
+		})
 
-    // 使用事務確保所有操作都成功或都失敗
-    await prisma.$transaction([
-      prisma.shared_cart_users.createMany({
-        data: [
-          {
-            shared_cart_group_id: sharedCartGroupId,
-            user_id: memberUserId.userId
-          },
-          {
-            shared_cart_group_id: sharedCartGroupId,
-            user_id: creatorUID,
-          }
-        ]
-      }),
+		// 使用事務確保所有操作都成功或都失敗
+		await prisma.$transaction([
+			prisma.shared_cart_users.createMany({
+				data: [
+					{
+						shared_cart_group_id: sharedCartGroupId,
+						user_id: memberUserId.userId,
+					},
+					{
+						shared_cart_group_id: sharedCartGroupId,
+						user_id: creatorUID,
+					},
+				],
+			}),
 
-      prisma.shared_carts.create({
-        data: {
-          group_id: sharedCartGroupId,
-          name: sharedCartName
-        }
-      })
-    ])
+			prisma.shared_carts.create({
+				data: {
+					group_id: sharedCartGroupId,
+					name: sharedCartName,
+				},
+			}),
+		])
 
-    res.json({ message: "創建共享購物車成功", groupId: sharedCartGroupId })
-  } catch (err) {
-    res.status(500).json({
-      message: "創建共享購物車失敗",
-      err: err.message
-    })
-  }
+		res.json({ message: "創建共享購物車成功", groupId: sharedCartGroupId })
+	} catch (err) {
+		res.status(500).json({
+			message: "創建共享購物車失敗",
+			err: err.message,
+		})
+	}
 })
 
-module.exports = router
+export default router
