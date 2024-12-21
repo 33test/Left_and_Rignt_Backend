8305fa303088c32f7931ff4d39d9b29ee733ed9c
cloverche <sunny40224@gmail.com>
2024-12-21 13:28:39 +0800

    add:從API獲取匯率資料,並存入資料庫

---
+++ src/routes/exchangeRate.js
@@ -0,0 +1,105 @@
+import express from "express"
+import prisma from "../configs/prisma.js"
+import axios from "axios"
+import { scheduleJob } from "node-schedule"
+
+const router = express.Router()
+const baseCurrency = "TWD"
+const API_KEY = process.env.EXCHANGE_RATE_API_KEY
+let counts = 10
+
+//從API取得匯率
+const getRate = async () => {
+  try {
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${baseCurrency}`)
+    counts++
+    return response.data.conversion_rates
+  } catch (err) {
+    throw new Error("資料拿取失敗")
+  }
+}
+
+//手動更新資料庫匯率資料
+const ratesUpdate = async (rateData) => {
+  try {
+    // 先檢查 rateData 是否有效
+    if (!rateData || typeof rateData !== "object") {
+      throw new Error("無效的匯率資料")
+    }
+
+    //查詢資料庫中的幣種
+    const existingCurrencies = (
+      await prisma.rates.findMany({
+        select: { currency: true },
+      })
+    ).map((item) => item.currency)
+
+    // 更新資料庫匯率
+    const updateRates = existingCurrencies
+      // 確認是否有API中沒有的幣種
+      .filter((currency) => rateData[currency] !== undefined)
+      .map((currency) =>
+        prisma.rates.update({
+          where: { currency },
+          data: {
+            rate: rateData[currency],
+            latestUpdateTime: new Date(),
+          },
+        })
+      )
+    if (updateRates.length === 0) {
+      throw new Error("沒有可更新的匯率資料")
+    }
+    await Promise.all(updateRates)
+  } catch (err) {
+    throw new Error(`資料更新失敗: ${err.message}`)
+  }
+}
+//定時更新資料庫匯率資料
+const scheduleRateUpdate = () => {
+  //每5分鐘更新一次
+  scheduleJob("*/5 * * * *", async () => {
+    try {
+      console.log("開始執行定時更新", new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }))
+
+      //拿到API匯率
+      const rateData = await getRate()
+
+      //資料庫更新
+      await ratesUpdate(rateData)
+
+      console.log({
+        message: "定時更新成功",
+        date: new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }),
+        counts,
+      })
+    } catch (err) {
+      console.log({
+        message: "定時更新失敗",
+        date: new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }),
+        error: err.message,
+        counts,
+      })
+    }
+  })
+}
+
+router.get("/", async (_req, res) => {
+  try {
+    //拿到API匯率
+    const rateData = await getRate()
+
+    //資料庫更新
+    await ratesUpdate(rateData)
+
+    res.status(200).json({
+      message: "更新完成",
+      counts,
+    })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+scheduleRateUpdate()
+export default router
